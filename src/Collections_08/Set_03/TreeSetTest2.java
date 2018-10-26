1e19b21a8c8eae63267470b593947ce1e7f39d96
harbor.M <1253880904@qq.com>
2018-10-26 17:59:06 +0800

    1026

--- src/Collections_08/Set_03/TreeSetTest2.java
+++ src/Collections_08/Set_03/TreeSetTest2.java
@@ -1,15 +1,35 @@
 package Collections_08.Set_03;
 
-class Z {
+import java.util.TreeSet;
+
+class Z implements Comparable {
     int age;
 
     public Z(int age) {
         this.age = age;
     }
+
+    @Override
+    public boolean equals(Object obj) {
+        return true;
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        return 1;
+    }
 }
 
 public class TreeSetTest2 {
     public static void main(String[] args) {
+        TreeSet treeSet = new TreeSet();
+        Z z1 = new Z(6);
+
+        treeSet.add(z1);
+        System.out.println(treeSet.add(z1));
+
+        System.out.print(treeSet);
+        ((Z) treeSet.first()).age = 6;
 
     }
 }
