069f6bf394536102a8d8337de66b4d6893a876a7
meihb <mhb_208@outlook.com>
2018-11-27 22:05:07 +0800

    1127 night

--- src/Jdbc/BlobTest.java
+++ src/Jdbc/BlobTest.java
@@ -2,8 +2,13 @@ package Jdbc;
 
 import javax.swing.*;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 
 public class BlobTest {
     JFrame jf = new JFrame("图片管理程序");
     private static Connection conn;
+    private static PreparedStatement insert;
+    private static PreparedStatement query;
+    private static PreparedStatement queryAll;
+    //定义一个DefaultListModel对象
 }
